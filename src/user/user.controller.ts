fbead7a660c20d4cd8448abaa3df585890d38d18
Overseer <manzaero>
2024-01-31 23:41:36 +0300

    first commit

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,7 +5,7 @@ import {UserService} from "./user.service";
 export class UserController {
     constructor(private readonly userService: UserService){}
 
-    @Get('all-users')
+    @Get()
     getUsers(){
         return this.userService.getUsers()
     }
